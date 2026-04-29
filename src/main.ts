cfacf0b2d1bf3ded02725c4a2f2dac0f6fa900e9
Prakhar <prakhar.s.srivastava@gmail.com>
2026-04-29 17:06:58 +0530

    working adaptive pan

--- src/main.ts
+++ src/main.ts
@@ -91,7 +91,7 @@ downloadJsonBtn.addEventListener("click", () => {
 randomizeBtn.addEventListener("click", () => {
   const startPrice = initialSeries.candles[0]?.o ?? 100;
   const startTime = initialSeries.candles[0]?.t ?? Date.now();
-  const length = 40 + Math.floor(Math.random() * 500); // 40..250
+  const length = 40 + Math.floor(Math.random() * 5000); // 40..250
   const next = generateRandomSeries({
     symbol: initialSeries.symbol,
     interval: initialSeries.interval,
